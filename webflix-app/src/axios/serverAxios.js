b7e5342a29daadd2b695a43a88ed5ecf21e749b2
Caolain Hession <c.hession9@universityofgalway.ie>
2024-03-09 01:17:07 +0000

    V 1.4 vercel

--- webflix-app/src/axios/serverAxios.js
+++ webflix-app/src/axios/serverAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverAxios = axios.create({
-    baseURL: 'webflix-app.vercel.app' 
+    baseURL: 'http://localhost:5000' 
   });
 
   export default serverAxios;
